aa6bc2149aece7cc78af9beb242622bc5d38bd72
IcedCaffeine <fahad7923@gmail.com>
2022-12-21 13:27:28 -0500

    Linked profile image directly to github repo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
   name: 'Fahad Abdul Rahman',
   title: "Computer Engineer",
   desciption: '"First, solve the problem. Then, write the code". -John Johnson',
-  image: 'https://media-exp1.licdn.com/dms/image/C4D03AQGPdznKuUgRsA/profile-displayphoto-shrink_800_800/0/1642871365723?e=1669248000&v=beta&t=kYPTIRzxYh4k18ocGmqRQpffcsoCrJNVN5pODPx9Ji0',
+  image:'https://github.com/IcedCaffeine/developer-portfolio/blob/master/src/assets/png/profilePicture.png?raw=true',
   resumePdf: resume
 }
